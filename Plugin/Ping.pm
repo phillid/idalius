13be301fab6a21e35b1203ced76e0036b8473e82
David Phillips <david@sighup.nz>
2018-04-10 16:22:15 +1200

    Add Ping module

---
+++ Plugin/Ping.pm
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+
+package Plugin::Ping;
+
+use strict;
+use warnings;
+
+my %config;
+
+sub configure {
+	my $self = shift;
+	my $cmdref = shift;
+
+	$cmdref->("ping", sub { $self->ping(@_); } );
+
+	return $self;
+}
+
+sub ping {
+	my ($self, $logger, $who, $where, $rest, @arguments) = @_;
+	my $nick = (split /!/, $who)[0];
+	return "$nick: pong";
+}
+1;
