e59093be7619f969c9e935d3de539e7432286942
David Phillips <david@sighup.nz>
2018-08-01 12:06:36 +1200

    Timezone: Implement case insensitivity
    Merges GitHub #14

--- Plugin/Timezone.pm
+++ Plugin/Timezone.pm
@@ -29,9 +29,10 @@ sub time {
 	return "Syntax: time [nick]" unless @arguments == 1;
 
 	my $nick = $arguments[0];
-	if (grep {$_ =~ /^$nick$/i} @known_zones) {
+	my ($case_nick) = grep {/^$nick$/i} @known_zones;
+	if ($case_nick) {
 		my $d = DateTime->now();
-		$d->set_time_zone($config{timezone}->{$nick});
+		$d->set_time_zone($config{timezone}->{$case_nick});
 		my $timestr = $d->strftime("%Y-%m-%d %H:%M %Z");
 		return "$requester: $nick\'s clock reads $timestr";
 	} else {
