ade3c69f9e307f27d789d4a3f5009230a56d554b
David Phillips <david@sighup.nz>
2018-09-09 22:50:28 +1200

    Add Jinx.pm

---
+++ Plugin/Jinx.pm
@@ -0,0 +1,38 @@
+package Plugin::Jinx;
+
+# Makes idalius join in on streaks of a person/some people saying the same
+# thing more than once in a row
+
+use strict;
+use warnings;
+
+# Last message we responded to with a jinx
+my $last_response = undef;
+
+# Last message said on the channel
+my $last = undef;
+my %config;
+
+sub configure {
+	my $self = $_[0];
+	my $cmdref = $_[1];
+	my $cref = $_[2];
+	%config = %$cref;
+	return $self;
+}
+
+sub message {
+	my ($self, $logger, $me, $who, $where, $raw_what, $what, $irc) = @_;
+
+	return if defined $last_response and $what eq $last_response;
+
+	if (defined $last and $last eq $what ) {
+		$last_response = $last;
+		return $last;
+	}
+
+	$last = $what;
+	$last_response = undef;
+	return;
+}
+1;
