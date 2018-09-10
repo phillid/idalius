8b479752eeb57daeaf2b9f0347fc0c46dd5d4fc5
David Phillips <david@sighup.nz>
2018-09-10 19:38:10 +1200

    Handle/respond to action vs message appropriately

--- Plugin/Jinx.pm
+++ Plugin/Jinx.pm
@@ -9,7 +9,7 @@ use warnings;
 # Last message we responded to with a jinx
 my $last_response = undef;
 
-# Last message said on the channel
+# Last message said on the channel, and whether it's action or message
 my $last = undef;
 my %config;
 
@@ -26,10 +26,30 @@ sub message {
 
 	return if defined $last_response and $what eq $last_response;
 
-	if (defined $last and $last eq $what ) {
+	if (defined $last and $last eq $what) {
 		$last_response = $last;
 		return $last;
 	}
+
+	$logger->("Storing $what");
+
+	$last = $what;
+	$last_response = undef;
+	return;
+}
+
+sub action {
+	my ($self, $logger, $me, $who, $where, $raw_what, $what, $irc) = @_;
+
+	return if defined $last_response and $what eq $last_response;
+
+	if (defined $last and $last eq $what) {
+		$last_response = $last;
+		$irc->yield(ctcp => $where->[0] => "ACTION" => $what);
+		return;
+	}
+
+	$logger->("Storing action $what");
 
 	$last = $what;
 	$last_response = undef;
