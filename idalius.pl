14d03ea7e78ef45f0842a50f3734ea146a90682b
David Phillips <david@sighup.nz>
2018-09-13 18:50:02 +1200

    Fix command ignore parenthesis

--- idalius.pl
+++ idalius.pl
@@ -205,8 +205,8 @@ sub handle_common {
 
 	my $stripped_what = strip_color(strip_formatting($what));
 	my $no_prefix_what = $stripped_what;
-	if (!should_ignore($nick) && $config{prefix_nick} && $no_prefix_what =~ s/^\Q$current_nick\E[:,]\s+//g ||
-	    $no_prefix_what =~ s/^$config{prefix}//) {
+	if (!should_ignore($nick) && ($config{prefix_nick} && $no_prefix_what =~ s/^\Q$current_nick\E[:,]\s+//g ||
+	    $no_prefix_what =~ s/^$config{prefix}//)) {
 		$output = run_command($no_prefix_what, $who, $where);
 		$irc->yield(privmsg => $where => $output) if $output;
 		strike_add($nick, $channel) if $output;
