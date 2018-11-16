10a8409ac7ef9388375e3b09da25a9c7e1931fef
David Phillips <david@yeah.nah.nz>
2018-11-16 15:05:50 +1300

    Men: Don't capture entire line

--- Plugin/Men.pm
+++ Plugin/Men.pm
@@ -23,7 +23,7 @@ sub on_message {
 		$where = $where->[0];
 	}
 
-	if ($what =~ /(\b.+men.+\b)/) {
+	if ($what =~ /(\w+men\w+)/) {
 		my ($w, $c, $target);
 		$w = $c = $target = $1;
 		$w =~ s/men/women/;
