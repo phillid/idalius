83788d77a7dc389c9e72e3e5431da0a906421a72
David Phillips <david@yeah.nah.nz>
2019-01-01 21:32:35 +1300

    Timezone: Default to requester's nick if none given

--- Plugin/Timezone.pm
+++ Plugin/Timezone.pm
@@ -27,15 +27,15 @@ sub time {
 	my $requester = (split /!/, $who)[0];
 	my @known_zones = (keys %{$config->{timezone}});
 
-	return "Syntax: time [nick]" unless @arguments == 1;
+	return "Syntax: time [nick]" unless @arguments <= 1;
 
-	my $nick = $arguments[0];
+	my $nick = $arguments[0] || $requester;
 	my ($case_nick) = grep {/^$nick$/i} @known_zones;
 	if ($case_nick) {
 		my $d = DateTime->now();
 		$d->set_time_zone($config->{timezone}->{$case_nick});
 		my $timestr = $d->strftime("%H:%M on %a %d %b, %Y (%Z)");
-		return "$requester: $nick\'s clock reads $timestr";
+		return "$nick\'s clock reads $timestr";
 	} else {
 		return "$requester: I don't know what timezone $nick is in";
 	}
